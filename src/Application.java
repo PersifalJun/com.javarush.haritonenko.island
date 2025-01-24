bc2562759b8df4de6b78331011ac7e6e71c0ab7b
Evgeniy Haritonenko <haritonenko04@mail.ru>
2025-01-24 17:32:40 +0700

    Изменено

--- src/Application.java
+++ src/Application.java
@@ -2,7 +2,8 @@
 import entity.Location;
 import util.Statistics;
 
-import java.util.Map;
+
+import java.sql.SQLOutput;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadLocalRandom;
@@ -11,58 +12,54 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class Application {
-    static Statistics statistics = new Statistics();
-
 
     public static void main(String[] args) throws InterruptedException {
 
         Location location = new Location(); // Создание локации
+        Statistics statistics = new Statistics(); //Статистика
 
         ScheduledExecutorService ses = Executors.newScheduledThreadPool(3); // Пул на 3 потока
-        ThreadLocalRandom random = ThreadLocalRandom.current();
 
+        AtomicInteger completedCycles = new AtomicInteger(0);
 
-        AtomicInteger cyclesQuantity = new AtomicInteger(0);
-
-        Runnable task = () -> {
+        Runnable locationTask = () -> {
             ThreadLocalRandom localRandom = ThreadLocalRandom.current();
             // Планируем задачи на каждый цикл
             for (int i = 0; i < location.lifeCycles; i++) {
+                if (!location.getAnimals().isEmpty()) {
+                    location.run();
+                    completedCycles.incrementAndGet();
+
+
+                } else {
+                    System.out.println("Локация пуста");
+                    System.out.println("Жизнь на острове завершена " + "совершено" + completedCycles.get() + " циклов.");
+                    ses.shutdown();
+                    return; // Прерываем выполнение
+                }
+            }
+            };
+
+            Runnable statisticsTask = statistics::run;
+
+            ses.scheduleAtFixedRate(locationTask, 0, 5, TimeUnit.SECONDS); // Запуск каждую секунду
+            ses.scheduleAtFixedRate(statisticsTask, 1, 5, TimeUnit.SECONDS); // Запуск статистики каждую секунду со смещением
+
+
+                //жидание завершения всех задач
                 ses.schedule(() -> {
-                    if (!location.getAnimals().isEmpty()) {
-
-
-                        ses.submit(location::animalAndPlantLifeCycle);
-                        ses.submit(statistics::printStatistics);
-
-
-                        statistics.printStatistics();
-                        cyclesQuantity.incrementAndGet();
+                    ses.shutdown();
+                    try {
+                        if (!ses.awaitTermination(10, TimeUnit.SECONDS)) {
+                            System.err.println("Некоторые задачи не успели завершиться вовремя");
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
-                    else{
-                        System.out.println("Локация пуста");
-                        System.out.println("Жизнь на острове завершена " + "совершено" + cyclesQuantity.get() + " циклов.");
-                        ses.shutdown();
-                        return; // Прерываем выполнение
-                    }
+                    System.out.println("Все задачи завершены");
+                }, location.lifeCycles * 5L, TimeUnit.SECONDS);
+            };
 
-                    statistics.printStatistics();
-                    cyclesQuantity.incrementAndGet();
+        }
 
-                    if (cyclesQuantity.get() >= location.lifeCycles) {
-                        ses.shutdown();
-                        System.out.println("Жизнь на острове завершена " + "совершено" + cyclesQuantity.get() + " циклов.");
-                    }
-                }, i * 5, TimeUnit.SECONDS); // Запускаем каждый цикл через 5 секунд
-            }
 
-            // Дожидаемся завершения работы executor-а
-            try {
-                ses.awaitTermination(location.lifeCycles * 5, TimeUnit.SECONDS);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        };
-
-    }
-}
